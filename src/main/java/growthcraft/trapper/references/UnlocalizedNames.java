6b0e2a4ac1e088b8ee4ce7f33f3208a07a1c37b6
Alatyami <alatyami@gmail.com>
2024-11-22 10:05:41 -0500

    Rename UnlocalizedName to UnlocalizedNames.
    Updated the class name and various identifiers for consistency and clarity. The changes include modified string constants and the addition of new identifiers.

---
+++ src/main/java/growthcraft/trapper/references/UnlocalizedNames.java
@@ -0,0 +1,44 @@
+package growthcraft.trapper.references;
+
+/**
+ * The UnlocalizedName class contains a series of static final strings that serve
+ * as unique identifiers for various traps and related components within the application.
+ * It includes identifiers for different types of traps, containers, and related resources.
+ * This class is not meant to be instantiated.
+ */
+public class UnlocalizedNames {
+    public static final String ANIMAL_TRAP = "animal_trap";
+    public static final String ANIMAL_TRAP_CONTAINER = "animal_trap_container";
+
+    public static final String ANIMAL_TRAP_COPPER = "animal_trap_copper";
+    public static final String ANIMAL_TRAP_DIAMOND = "animal_trap_diamond";
+    public static final String ANIMAL_TRAP_GOLD = "animal_trap_gold";
+    public static final String ANIMAL_TRAP_IRON = "animal_trap_iron";
+
+    public static final String FISH_TRAP = "fish_trap";
+    public static final String FISH_TRAP_CONTAINER = "fish_trap_container";
+    public static final String FISH_TRAP_OAK = "fish_trap_oak";
+    public static final String FISH_TRAP_ACACIA = "fish_trap_acacia";
+    public static final String FISH_TRAP_SPRUCE = "fish_trap_spruce";
+    public static final String FISH_TRAP_JUNGLE = "fish_trap_jungle";
+    public static final String FISH_TRAP_DARK_OAK = "fish_trap_dark_oak";
+    public static final String FISH_TRAP_BIRCH = "fish_trap_birch";
+    public static final String FISH_TRAP_BAMBOO = "fish_trap_bamboo";
+    public static final String FISH_TRAP_CHERRY = "fish_trap_cherry";
+    public static final String FISH_TRAP_CRIMSON = "fish_trap_crimson";
+    public static final String FISH_TRAP_MANGROVE = "fish_trap_mangrove";
+    public static final String FISH_TRAP_WARPED = "fish_trap_warped";
+
+    public static final String SPAWN_EGG_TRAP = "spawn_egg_trap";
+    public static final String SPAWN_EGG_TRAP_NETHERITE = "spawn_egg_trap_netherite";
+
+    public static final String TAG_FISHING_BAIT = "fishing_bait";
+
+    public static final String SOUND_FISH_TRAP_OPEN = "fish_trap_open";
+    public static final String CREATIVE_TAB = "tab";
+    public static final String CREATIVE_TAB_KEY = "item_group.growthcraft_trapper.tab";
+
+    private UnlocalizedNames() {
+        /* Prevent automatic implementation of public constructor */
+    }
+}
